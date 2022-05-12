9c286b82da6850fed4c6d2b5a3a62484f5f1698c
Adrian Bona <adrianbona@outlook.com>
2022-05-12 14:36:18 -0300

    Quotes and missing dependencies

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from 'react-redux'
-import store from './redux/store';
-import 'semantic-ui-less/semantic.less';
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import "semantic-ui-less/semantic.less";
 import "tailwind/tailwind.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
